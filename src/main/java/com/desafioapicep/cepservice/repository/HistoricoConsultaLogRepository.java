9eb616c231ad9adf6d677ebf575d29e309b6a063
mmonteiro91 <mmonteirodev@gmail.com>
2025-01-13 12:50:26 -0300

    1 - adicionando querys

--- src/main/java/com/desafioapicep/cepservice/repository/HistoricoConsultaLogRepository.java
+++ src/main/java/com/desafioapicep/cepservice/repository/HistoricoConsultaLogRepository.java
@@ -1,5 +1,7 @@
 package com.desafioapicep.cepservice.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,9 @@ import com.desafioapicep.cepservice.entity.HistoricoConsulta;
 
 @Repository
 public interface HistoricoConsultaLogRepository extends JpaRepository<HistoricoConsulta, Long> {
+
+	List<HistoricoConsulta> findByCep(String cep);
+	
+	List<HistoricoConsulta> getByIdAndCe(Long id, String cep);
+
 }
